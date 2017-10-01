4ebed8be21ab8c16c8e5977cbb7e2b82f1a58b40
Pratik Chaudhari <chaudharipratikg@gmail.com>
2017-10-01 18:06:22 +0530

    Added guess-the-number

---
+++ guess-the-number/src/main.rs
@@ -0,0 +1,38 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+
+    println!("Guess the number");
+    println!("---------------------------------");
+
+    let secret_number = rand::thread_rng().gen_range(1,100);
+
+    loop{
+        println!("Please enter your guess:");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{} is an invalid number!",guess.trim());
+                continue;
+            }
+        };
+        println!("You guessed {}",guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("Just right!!");
+                break;
+            }
+        }
+    }
+    
+}
